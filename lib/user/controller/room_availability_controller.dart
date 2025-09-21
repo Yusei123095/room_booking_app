33b797c1527161c45095c09b38b5c9d3ce7982d4
Yusei <yu43554334@gmail.com>
2025-09-22 02:11:02 +0800

    This changes reflect new data type of timeslots, and consider modification of booking

--- lib/user/controller/room_availability_controller.dart
+++ lib/user/controller/room_availability_controller.dart
@@ -6,7 +6,7 @@ import 'package:room_booking_app/utils/time_utils.dart';
 
 final roomAvailabilityProvider = FutureProvider.family<
   bool?,
-  ({bool isTemp, String? roomId})
+  ({bool isTemp, String? roomId, String? bookId})
 >((ref, arg) async {
   final bookingDateTimeState = ref.watch(bookingDateProvider);
   final bookingDateTimeNotifier = ref.watch(bookingDateProvider.notifier);
@@ -14,22 +14,20 @@ final roomAvailabilityProvider = FutureProvider.family<
   final endSelectedDate = selectedDate.add(const Duration(days: 1));
 
   final selectedTimeslots = bookingDateTimeNotifier.getTimeslots(arg.isTemp);
-  final selectedTimeslotsNumbers = selectedTimeslots.map((item) => item['number']);
-
+  final timeslotsDetail = bookingDateTimeState.timeslotDetails;
   if (selectedTimeslots.isEmpty) {
 
     return null;
   }
 
-  int startHour = selectedTimeslots[0]['start_hour'];
-  int startMin = selectedTimeslots[0]['start_min'];
+  int startHour = timeslotsDetail[selectedTimeslots[0]]!['start_hour'];
+  int startMin = timeslotsDetail[selectedTimeslots[0]]!['start_min'];
 
   if (checkPassedTime(selectedDate, startHour, startMin)) {
     return false;
   }
 
   if (arg.roomId == null) {
-
     return true;
   }
 
@@ -39,9 +37,10 @@ final roomAvailabilityProvider = FutureProvider.family<
           .where("room", isEqualTo: arg.roomId)
           .where("date", isGreaterThanOrEqualTo: selectedDate)
           .where("date", isLessThan: endSelectedDate)
-          .where("timeslots", arrayContainsAny: selectedTimeslotsNumbers)
+          .where("timeslots", arrayContainsAny: selectedTimeslots)
           .get();
 
+  final filteredBooks = books.docs.where((book) => book.id != arg.bookId).toList();
 
-  return books.docs.isEmpty;
+  return filteredBooks.isEmpty;
 });
